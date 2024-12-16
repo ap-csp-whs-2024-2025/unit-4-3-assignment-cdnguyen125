6f811eed3d1a2de537bbcc1144cf00e41715cef9
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2024-12-16 21:24:28 +0000

    help

--- calculator.cpp
+++ calculator.cpp
@@ -41,6 +41,15 @@ int main()
         std::cout << "Their sum is " << (num1 + num2) << std::endl;
       }
     }
+    else
+    {
+      if (user_input == 2)
+      {
+        std::cout << "Enter two numbers" << std::endl;
+        std::cin >> num1 >> num2;
+        std::cout << "Their difference is" << (num1 - num2) << std::endl;
+      }
+    }
   }
   return 0;
 }
